b7cf5ceee76cba4b34dcca68fca81d2232c5eac8
tannineo <tannineo@outlook.com>
2022-05-19 17:29:23 +0100

    update to trigger

--- packages/app/src/components/Home.tsx
+++ packages/app/src/components/Home.tsx
@@ -5,6 +5,8 @@ const Home = () => {
     <div className='flex flex-col font-bold items-center justify-start text-3xl underline w-full'>
       <h1>Number Converter</h1>
 
+      <h2>23333</h2>
+
       <img className='w-56' src={Logo} />
     </div>
   )
